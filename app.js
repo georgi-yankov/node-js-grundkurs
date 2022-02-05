d30d80a916107d8e5608a67904b60c56c96db183
Georgi Yankov <georgi.st.yankov@gmail.com>
2022-02-05 11:36:43 +0200

    convert indentation to spaces

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.static('public'));
 
 // app.get('/', (request, response) => {
-// 	response.send('<h1>Hallo Welt!</h1>');
+//  response.send('<h1>Hallo Welt!</h1>');
 // });
 
 app.listen(3002, () => {
-	console.log('Webserver laeuft unter port 3002');
+  console.log('Webserver laeuft unter port 3002');
 });
